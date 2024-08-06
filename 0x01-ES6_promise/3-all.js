935f083f2643e4ca768f3be73c709c3e842e02e7
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-07 07:36:08 +0800

    Correction Exo 3-all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils'
 
 export default function handleProfileSignup() {
+  let body, firstName, lastName;
   return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
-      let body, firstName, lastName;
       ({ body } = value[0]);
       ({ firstName, lastName } = value[1]);
       console.log(body, firstName, lastName);
