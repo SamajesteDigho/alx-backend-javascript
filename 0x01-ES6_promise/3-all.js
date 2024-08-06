030727c2958ec60c3d1c863d627c017ab7343362
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-07 07:46:25 +0800

    Correction Exo 3-all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       ({ body } = value[0]);
-      ({ firstName, lastName } = value[1]);
+      ({ firstName, lastName, } = value[1]);
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => { console.log('Signup system offline'); });
