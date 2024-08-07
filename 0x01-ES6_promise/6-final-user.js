315eb331a7091603f801ed356fc2778707ce55d0
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-07 08:04:15 +0800

    Correction Exo 6-final_user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,6 @@ import uploadPhoto from './5-photo-reject'
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((value) => { return value; });
+    .allSettled([signUpUser(uploadPhoto(filename), firstName, lastName)])
+    .then((value) => value);
 }
