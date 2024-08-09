60f196637777974375ff3323a72412f1257c0dd2
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-09 15:06:44 +0800

    Commiting

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,7 @@
-import { uploadPhoto, createUser } from './utils'
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let result = await Promise.allSettled([uploadPhoto(), createUser()]);
+  const result = await Promise.allSettled([uploadPhoto(), createUser()]);
   for (const { status } of result) {
     if (status !== 'fulfilled') {
       return { photo: null, user: null };
