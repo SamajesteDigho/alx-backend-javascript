26a581dd0b32746346d99f785c35b8bf95e7715e
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-12 15:33:24 +0800

    Correcting Exo 0x01 ES6 promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser, } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   let body;
@@ -6,9 +6,9 @@ export default function handleProfileSignup() {
   let lastName;
   return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
-      let [ first, second ] = value;
-      ({ body, } = first);
-      ({ firstName, lastName, } = second);
+      const [first, second] = value;
+      ({ body } = first);
+      ({ firstName, lastName } = second);
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => { console.log('Signup system offline'); });
