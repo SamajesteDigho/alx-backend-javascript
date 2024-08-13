57b0c2d6b1e47ec8b3fe47414417b4740326231f
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-13 08:57:35 +0800

    Correcting Exo 0x03 ES6 data manipulation

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(objects, city) {
   if (!Array.isArray(objects)) {
-    return Array();
+    return [];
   }
   return objects.filter((obj) => obj.location === city);
 }
--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(objects, city, newGrades) {
   return objects.filter((obj) => obj.location === city).map((obj) => {
-    let grade = newGrades.find((grd) => grd.studentId === obj.id);
+    const grade = newGrades.find((grd) => grd.studentId === obj.id);
     grade ? obj.grade = grade.grade : obj.grade = 'N/A';
     return obj;
   });
