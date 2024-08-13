abb6478b935fa729c3f95664c44d09d5a970d9e9
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-13 08:50:15 +0800

    Correcting Exo 0x03 ES6 data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,5 @@ export default function getListStudentIds(objects) {
   if (!Array.isArray(objects)) {
     return Array();
   }
-  const ids = [];
-  objects.map((obj) => ids.push(obj.id));
-  return ids;
+  return objects.map((obj) => obj.id);
 }
--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,15 +1,5 @@
-import { queryAPI, weakMap } from "./100-weak.js";
+import getListStudentIds from "./1-get_list_student_ids.js";
+import getListStudents from "./0-get_list_students.js";
 
-const endpoint = { protocol: 'http', name: 'getUsers' };
-weakMap.get(endpoint);
-
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
-
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
-
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
+console.log(getListStudentIds("hello"));
+console.log(getListStudentIds(getListStudents()));
