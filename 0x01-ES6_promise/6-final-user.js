dddb25e4242dbb7671934f41cb1865e69059d38d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-14 21:57:51 +0800

    Correction Exo 0x01-ES6_promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,14 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((value) => value);
+    .then((value) => value.map((elt) => {
+      let obj = {};
+      if (elt.status === 'rejected') {
+        obj.status = elt.status;
+        obj.value = elt.reason.message;
+      } else {
+        obj = { ...elt };
+      }
+      return obj;
+    }));
 }
