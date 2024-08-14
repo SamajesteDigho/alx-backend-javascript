0286dd1d526391ec83648407968cddf7040f043d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-14 19:23:58 +0800

    Correction Exo 0x03 ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,7 +4,7 @@ export function queryAPI(epa) {
   const nb = weakMap.get(epa);
   if (!nb) {
     weakMap.set(epa, 1);
-  } else if (nb >= 5) {
+  } else if (nb >= 4) {
     throw new Error('Endpoint load is high');
   } else {
     weakMap.set(epa, nb + 1);
