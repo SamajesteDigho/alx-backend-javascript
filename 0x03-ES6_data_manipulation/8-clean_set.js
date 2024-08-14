2e2e6dc0faf8aec3ea92823f18f872ad5fa7f789
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-14 14:17:19 +0800

    Correction of 0x03 ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (startString.length == 0) {
+  if (startString.length === 0) {
     return '';
   }
   const match = [];
-  set.forEach((elt) => elt.startsWith(startString)
-  ? match.push(elt.replace(startString, '')) : null); 
+  set.forEach((elt) => {
+    if (elt.startsWith(startString)) {
+      match.push(elt.replace(startString, ''));
+    }
+  });
   return match.join('-');
 }
