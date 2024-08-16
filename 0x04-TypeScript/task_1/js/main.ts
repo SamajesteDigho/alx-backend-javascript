ea18c073128ea55f81d3c3226d6f00e9a11c48ef
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-16 17:34:54 +0800

    Pushing ignore file

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,43 @@
-export default interface Teacher {
+/** Exxo 1 */
+export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
   [key: string]: any;
+}
+
+/** Exo 2 */
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+/** Exo 3 */
+interface PrintTeacherFunction { (firstName: string, lastName: string): string }
+
+const printTeacher: PrintTeacherFunction = function (
+  firstName: string,
+  lastName: string
+) {
+  return `${firstName.at(0)}. ${lastName}`;
+};
+
+/** Exo 4 */
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomeWork(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
