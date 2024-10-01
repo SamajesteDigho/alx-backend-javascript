93b57e8d4d0e4564be3e0903c1620d212e14c017
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-01 12:04:35 +0800

    Commiting

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents (path) {
+async function countStudents (path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
     let NUMBER_OF_STUDENTS = 0;
