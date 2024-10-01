40177cc00ee2930c013ff35295bf5d1e42c71d57
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-01 12:35:47 +0800

    Commiting

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-async function countStudents (path) {
+function countStudents (path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
     let NUMBER_OF_STUDENTS = 0;
--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,31 +1,36 @@
 const fs = require('fs');
 
-export default function countStudents (path) {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    } else {
-      let NUMBER_OF_STUDENTS = 0;
-      let lines = data.split('\n');
-      if (lines.length !== 0 && lines[lines.length - 1].length === 0) {
-        lines.pop();
-      }
-      lines = lines.slice(1);
-      const FIELDS = {};
-
-      lines.forEach(elt => {
-        NUMBER_OF_STUDENTS += 1;
-        const parts = elt.split(',');
-        if (!Object.keys(FIELDS).includes(parts[3])) {
-          FIELDS[parts[3]] = [];
+async function countStudents (path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+      } else {
+        let NUMBER_OF_STUDENTS = 0;
+        let lines = data.split('\n');
+        if (lines.length !== 0 && lines[lines.length - 1].length === 0) {
+          lines.pop();
         }
-        FIELDS[parts[3]].push(parts[0]);
-      });
+        lines = lines.slice(1);
+        const FIELDS = {};
 
-      process.stdin.write(`Number of students: ${NUMBER_OF_STUDENTS}\n`);
-      Object.keys(FIELDS).forEach(elt => {
-        process.stdout.write(`Number of students in ${elt}: ${FIELDS[elt].length}. List: ${FIELDS[elt]}\n`);
-      });
-    }
+        lines.forEach(elt => {
+          NUMBER_OF_STUDENTS += 1;
+          const parts = elt.split(',');
+          if (!Object.keys(FIELDS).includes(parts[3])) {
+            FIELDS[parts[3]] = [];
+          }
+          FIELDS[parts[3]].push(parts[0]);
+        });
+
+        process.stdin.write(`Number of students: ${NUMBER_OF_STUDENTS}\n`);
+        Object.keys(FIELDS).forEach(elt => {
+          process.stdout.write(`Number of students in ${elt}: ${FIELDS[elt].length}. List: ${FIELDS[elt]}\n`);
+        });
+        resolve();
+      }
+    });
   });
 }
+
+module.exports = countStudents;
