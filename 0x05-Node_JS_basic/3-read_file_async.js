f204d44bfa88fac6c17a2dd394ac00f3f84bc124
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-01 13:19:57 +0800

    Commiting

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,6 +6,7 @@ async function countStudents (path) {
       if (err) {
         reject(Error('Cannot load the database'));
       } else {
+        let string = '';
         let NUMBER_OF_STUDENTS = 0;
         let lines = data.split('\n');
         if (lines.length !== 0 && lines[lines.length - 1].length === 0) {
@@ -23,11 +24,13 @@ async function countStudents (path) {
           FIELDS[parts[3]].push(parts[0]);
         });
 
+        string += `Number of students: ${NUMBER_OF_STUDENTS}\n`;
         process.stdin.write(`Number of students: ${NUMBER_OF_STUDENTS}\n`);
         Object.keys(FIELDS).forEach(elt => {
+          string += `Number of students in ${elt}: ${FIELDS[elt].length}. List: ${FIELDS[elt]}\n`;
           process.stdout.write(`Number of students in ${elt}: ${FIELDS[elt].length}. List: ${FIELDS[elt]}\n`);
         });
-        resolve();
+        resolve(string);
       }
     });
   });
