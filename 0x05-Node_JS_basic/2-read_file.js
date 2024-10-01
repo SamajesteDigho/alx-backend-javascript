392c89ec8a3befb9b2d666b8ab52e8717c272706
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-01 11:59:40 +0800

    Commiting

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,33 +1,32 @@
 const fs = require('fs');
 
 function countStudents (path) {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    } else {
-      let NUMBER_OF_STUDENTS = 0;
-      let lines = data.split('\n');
-      if (lines.length !== 0 && lines[lines.length - 1].length === 0) {
-        lines.pop();
-      }
-      lines = lines.slice(1);
-      const FIELDS = {};
-
-      lines.forEach(elt => {
-        NUMBER_OF_STUDENTS += 1;
-        const parts = elt.split(',');
-        if (!Object.keys(FIELDS).includes(parts[3])) {
-          FIELDS[parts[3]] = [];
-        }
-        FIELDS[parts[3]].push(parts[0]);
-      });
-
-      process.stdin.write(`Number of students: ${NUMBER_OF_STUDENTS}\n`);
-      Object.keys(FIELDS).forEach(elt => {
-        process.stdout.write(`Number of students in ${elt}: ${FIELDS[elt].length}. List: ${FIELDS[elt]}\n`);
-      });
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    let NUMBER_OF_STUDENTS = 0;
+    let lines = data.split('\n');
+    if (lines.length !== 0 && lines[lines.length - 1].length === 0) {
+      lines.pop();
     }
-  });
+    lines = lines.slice(1);
+    const FIELDS = {};
+
+    lines.forEach(elt => {
+      NUMBER_OF_STUDENTS += 1;
+      const parts = elt.split(',');
+      if (!Object.keys(FIELDS).includes(parts[3])) {
+        FIELDS[parts[3]] = [];
+      }
+      FIELDS[parts[3]].push(parts[0]);
+    });
+
+    process.stdin.write(`Number of students: ${NUMBER_OF_STUDENTS}\n`);
+    Object.keys(FIELDS).forEach(elt => {
+      process.stdout.write(`Number of students in ${elt}: ${FIELDS[elt].length}. List: ${FIELDS[elt]}\n`);
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
