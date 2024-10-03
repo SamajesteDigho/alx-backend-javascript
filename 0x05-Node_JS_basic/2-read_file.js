75a9eab9ded34b428c812f4de45e4bb390e5f4b0
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-03 10:37:53 +0800

    Commiting

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
     let NUMBER_OF_STUDENTS = 0;
@@ -11,7 +11,7 @@ function countStudents (path) {
     lines = lines.slice(1);
     const FIELDS = {};
 
-    lines.forEach(elt => {
+    lines.forEach((elt) => {
       NUMBER_OF_STUDENTS += 1;
       const parts = elt.split(',');
       if (!Object.keys(FIELDS).includes(parts[3])) {
@@ -21,7 +21,7 @@ function countStudents (path) {
     });
 
     process.stdin.write(`Number of students: ${NUMBER_OF_STUDENTS}\n`);
-    Object.keys(FIELDS).forEach(elt => {
+    Object.keys(FIELDS).forEach((elt) => {
       process.stdout.write(`Number of students in ${elt}: ${FIELDS[elt].length}. List: ${FIELDS[elt]}\n`);
     });
   } catch (err) {
