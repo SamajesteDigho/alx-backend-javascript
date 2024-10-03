52fa6a537c35ba1424217a5c45181466faaeca42
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-03 10:33:03 +0800

    Commiting

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
 
 process.stdin.on('data', (data) => {
   if (process.stdout.writable) {
-    console.log(`Your name is: ${data}`);
+    process.stdout.write(`Your name is: ${data}`);
   }
 });
