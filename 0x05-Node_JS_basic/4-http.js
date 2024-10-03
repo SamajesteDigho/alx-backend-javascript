dac64092a96679398060c30b7f31cdaea8bba45d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-03 11:06:45 +0800

    Commiting

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const { createServer } = require('node:http');
 const HOSTNAME = 'localhost';
 const PORT = '1245';
 
-const app = createServer((req, res) => {
+const app = createServer((_, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
