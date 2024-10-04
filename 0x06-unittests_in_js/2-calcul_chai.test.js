d1a93990199364ffefa957239b54109bbc53c80b
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-04 08:27:17 +0800

    Commiting

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,13 +1,13 @@
 const chai = require('chai');
 
 const expect = chai.expect;
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('test cases', () => {
   it('rounded typed', () => {
-    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.deep.equal(6);
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.deep.equal(-4);
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.deep.equal(0.2);
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.deep.equal('Error');
   });
 });
