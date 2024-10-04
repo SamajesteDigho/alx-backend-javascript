1ea7e98a44eb876da39017309aec77845919b167
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-10-04 09:14:44 +0800

    Commiting

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,7 +7,10 @@ const expect = chai.expect;
 
 describe('test cases', () => {
   it('test the rounding', () => {
-    const spy = sinon.spy(sendPaymentRequestToApi(100, 20));
-    expect(spy).to.deep.equal(Utils.calculateNumber('SUM', 100, 20));
+    const spy = sinon.spy(sendPaymentRequestToApi);
+    spy(100, 20);
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith(100, 20)).to.be.true;
+    expect(spy.returnValue).to.deep.equal(Utils.calculateNumber('SUM', 100, 20));
   });
 });
